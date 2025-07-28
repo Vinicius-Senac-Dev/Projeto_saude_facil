f609e8abd4068603a9091c2cd0c56a1d911cca44
viniciusK2 <127550204+viniciusK2@users.noreply.github.com>
2025-07-28 17:06:24 -0300

    admin
    atualizando arquivos

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saude_facil.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cadastro.settings')
     try:
-        from django.core.management import execute_from_command_line 
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
