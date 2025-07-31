c5348364781ea4945ccaac5256c1c1e17ef115e6
viniciusK2 <127550204+viniciusK2@users.noreply.github.com>
2025-07-31 16:53:31 -0300

    Delete admin directory

--- admin/usuarios/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsuariosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usuarios'
-    #Preciso instalar o meu app signals no meu projeto usuarios para funcionar.
-    def ready(self):
-        import usuarios.signals
--- admin/usuarios/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('cadastrar_recepcionista', views.cadastrar_recepcionista, name="cadastrar_recepcionista"),
-    path('login/', views.login, name='login')
-]
